67e6dcb9dfb585dc946f3f1264c0e92d00596e05
Alfarih Faza <alfarihfz@gmail.com>
2022-06-05 18:08:32 +0700

    fix: don't show email verify on verified

--- routes/request-email-verify.svelte
+++ routes/request-email-verify.svelte
@@ -2,6 +2,7 @@
 	import Paper, { Content } from '@smui/paper';
 	import Button from '@smui/button';
 	import { showSnackbar } from '$lib/stores/actions';
+	import { userState } from '$lib/stores';
 
 	let disableResendEmail = false;
 	let resendCountdown = 0;
@@ -31,21 +32,28 @@
 	};
 </script>
 
+<div class="mdc-typography--headline3" style="margin: 1rem">Verify Email</div>
 <Paper class="cms-dashboard__paper">
 	<Content>
-		<div class="mdc-typography--headline6">Oops! Your email not verified</div>
-		<div class="FlexContainer FlexContainer--Center--Col">
-			<Button disabled={disableResendEmail} on:click={resendVerificationEmail}>
-				{#if resendCountdown > 0}
-					{`Check your mailbox! you can retry in ${resendCountdown}`}
-				{:else}
-					Resend email verification
-				{/if}
-			</Button>
-			<Button on:click|once={handleHardReload}>
-				if you have verify your email or it was a mistake, you can reload this page now!
-				<div class="material-icons">replay</div>
-			</Button>
-		</div>
+		{#if $userState.profile.id && $userState.profile.provider === 'auth0' && !$userState.profile.email_verified}
+			<div class="mdc-typography--headline6">Oops! Your email not verified</div>
+			<div class="FlexContainer FlexContainer--Center--Col">
+				<Button disabled={disableResendEmail} on:click={resendVerificationEmail}>
+					{#if resendCountdown > 0}
+						{`Check your mailbox! you can retry in ${resendCountdown}`}
+					{:else}
+						Resend email verification
+					{/if}
+				</Button>
+				<Button on:click|once={handleHardReload}>
+					if you have verify your email or it was a mistake, you can reload this page now!
+					<div class="material-icons">replay</div>
+				</Button>
+			</div>
+		{:else}
+			<div class="mdc-typography--headline6 FlexContainer FlexContainer--Center">
+				Email verified <div class="material-icons">check</div>
+			</div>
+		{/if}
 	</Content>
 </Paper>
