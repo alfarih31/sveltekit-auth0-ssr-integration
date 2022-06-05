afa15b9de1a9bfbc0fe81daa8f1256e21f6c63ff
Alfarih Faza <alfarihfz@gmail.com>
2022-06-05 14:25:39 +0700

    fix: wrong sroute.config import

--- lib/components/Error.svelte
+++ lib/components/Error.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import Button, { Label } from '@smui/button';
 	import { Icon } from '@smui/icon-button';
-	import { homePath } from '$configs/client/sroute.config';
+	import { homePath } from '$configs/client/route.config';
 
 	const ErrorMessageMap: { [key: number]: string } = {
 		100: 'Continue',
